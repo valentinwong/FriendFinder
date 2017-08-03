b442b102dbb2c732d3ad4b602811419b10e4be92
Val Wong <valentin.wong@gmail.com>
2017-08-02 22:05:27 -0700

    update all files

---
+++ server.js
@@ -0,0 +1,52 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var users = [];
+
+//html routes
+app.get("/",function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+//api routes
+app.get("/api/friends",function(req,res){
+  res.json(users);
+})
+
+app.post("/api/friends", function(req,res){
+  var newUser = req.body;
+  users.push(newUser);
+});
+
+//listening
+app.listen(PORT, function(){
+  console.log("listening on port: " + PORT);
+})
